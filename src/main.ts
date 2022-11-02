71bf2d6d2e78209707ac3ebb98149a32fb6f317e
Pablo Ibarra <pabloo_ibarra98@hotmail.com>
2022-11-02 13:52:01 -0600

    websocket ready

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { connectToServer } from './socket-client';
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h2>Websocket - Client</h2>
+
+    <input id="jwt-input" placeholder="Json Web Token" />
+    <button id="btn-connect">Connect</button>
+
+    <br /><br />
+    <span id="server-status">Offline</span>
+
+    <ul id="clients-list"></ul>
+
+    <form id="message-form">
+      <input placeholder="message" id="message-input" />
+    </form>
+
+    <h3>Messages</h3>
+    <ul id="messages-list"></ul>
+  </div>
+`
+
+const jwtInput = document.querySelector<HTMLInputElement>('#jwt-input')!;
+const btnConnect = document.querySelector<HTMLButtonElement>('#btn-connect')!;
+
+btnConnect.addEventListener('click', () => {
+  if (jwtInput.value.trim().length <= 0) return alert('Enter a valid JWT');
+
+  connectToServer(jwtInput.value);
+});
